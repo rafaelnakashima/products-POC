398de79f66f7ea690a3f7e1a47a80687857d6588
Rafael Nakashima <rafael.nakashima@lexartlabs.xyz>
2023-09-12 17:50:31 -0300

    feat: added Product Service with getAll and CreateProduct methods

---
+++ backend/src/products/product.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from './entity/product.entity';
+import { Repository } from 'typeorm';
+import { NewProductDto } from './dto/new-product.dto';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private productsRepository: Repository<ProductEntity>,
+  ) {}
+
+  async getAll() {
+    return this.productsRepository.find();
+  }
+
+  async createProduct(data: NewProductDto) {
+    if (
+      await this.productsRepository.exist({
+        where: {
+          description: data.description,
+        },
+      })
+    ) {
+      throw new BadRequestException('produto já existente');
+    }
+
+    const product = this.productsRepository.create(data);
+    return this.productsRepository.save(product);
+  }
+}
