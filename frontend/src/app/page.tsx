1a8982ad9ad57b2af8a13301d2b0850e7a511499
Rafael Nakashima <rafael.nakashima@lexartlabs.xyz>
2023-09-15 11:27:56 -0300

    feat: Added button to push to /checkout, and field showing total on main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 import ProductCard from "../components/ProductCard";
 import Product from "../interfaces/product.interface";
 import { useMutation, useQuery } from '@apollo/client';
+import { useCart } from "../context/CartContext";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
   const { data, client, loading } = useQuery<{ getAllProducts: Product[] }>(GET_ALL_PRODUCTS);
+  const { cart, total } = useCart();
 
   const [deleteProduct] = useMutation(DELETE_PRODUCT);
 
@@ -32,6 +34,12 @@ export default function Home() {
                 Adicinonar novo produto
               </button>
             </Link>
+            <Link href="/checkout">
+              <button className="text-indigo-500 hover:text-indigo-600 border p-2 mx-2">
+              Ir para o checkout ({cart.length} items)
+              </button>
+            </Link>
+            {`Total cart: ${total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`}
           </div>
           <div className="md:grid grid-cols-4 gap-4 md:mx-20">
             {data?.getAllProducts.map((product: Product) => (
