e881103e8f86ee3764e8bc926ad8540882556edd
Rafael Nakashima <rafael.nakashima@lexartlabs.xyz>
2023-09-15 17:34:11 -0300

    feat: added fixed header component on main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
 import { DELETE_PRODUCT, GET_ALL_PRODUCTS } from "../lib/queries";
-import Link from "next/link";
 import ProductCard from "../components/ProductCard";
 import Product from "../interfaces/product.interface";
 import { useMutation, useQuery } from '@apollo/client';
-import { useEffect, useState } from "react";
-import { onAuthStateChanged, signOut, Auth } from "firebase/auth";
-import { auth } from "../firebase/config";
-import { useRouter } from 'next/navigation';
-import { useCart } from "../context/CartContext";
+import Header from "../components/Header";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
   const { data, client, loading } = useQuery<{ getAllProducts: Product[] }>(GET_ALL_PRODUCTS);
-  const { cart, total } = useCart();
-
   const [deleteProduct] = useMutation(DELETE_PRODUCT);
-  const [user, setUser] = useState<Auth['currentUser'] | null>(null); // Tipo definido para 'user'
 
-  const router = useRouter();
 
   const handleDelete = async (id: number) => {
     await deleteProduct({
@@ -31,53 +22,11 @@ export default function Home() {
     client.resetStore()
   };
 
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      setUser(null);
-      router.push("/login");
-    } catch (error) {
-      console.error("Erro ao fazer logout:", error);
-    }
-  };
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
-      }
-    });
-    return () => unsubscribe();
-  }, []);
-
   return (
     <>
+      <Header />
       {loading ? (<div>...Loading</div>) :
-        <main className='m-2'>
-          <div className='my-2'>
-            {!user ? (
-              <div>
-                <Link href={'/login'}>
-                  <p>login</p>
-                </Link>
-              </div>) : (
-              <div>
-                <p>Usuário logado: {user?.email}</p>
-                <button onClick={handleLogout}>Log off</button>
-              </div>
-            )}
-            <Link href="/new-product">
-              <button className="text-indigo-500 hover:text-indigo-600 border p-2">
-                Adicionar novo produto
-              </button>
-            </Link>
-            <Link href="/checkout">
-              <button className="text-indigo-500 hover:text-indigo-600 border p-2 mx-2">
-                Ir para o checkout ({cart.length} items)
-              </button>
-            </Link>
-            {`Total cart: ${total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`}
-          </div>
+        <main className='mt-24 md:mt-28'>
           <div className="md:grid grid-cols-4 gap-4 md:mx-20">
             {data?.getAllProducts.map((product: Product) => (
               <ProductCard
@@ -92,3 +41,4 @@ export default function Home() {
     </>
   );
 }
+
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,81 @@
+import Link from "next/link";
+import { useCart } from "../context/CartContext";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut, Auth } from "firebase/auth";
+import { auth } from "../firebase/config";
+import { useRouter } from 'next/navigation';
+
+
+
+const Header: React.FC = () => {
+  const { cart, total } = useCart();
+  const [user, setUser] = useState<Auth['currentUser'] | null>(null); // 
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+      router.push("/login");
+    } catch (error) {
+      console.error("Erro ao fazer logout:", error);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 right-0 bg-white p-4 border-gray-300 z-10 h-22 md:mx-20 border mt-2">
+      <div className="grid grid-cols-4 items-center text-center font-bold text-sm md:text-lg">
+        <div className="col-span-1 text-center">
+          {!user ? (
+            <Link href={'/login'}>
+              <p className="text-indigo-500 hover:text-indigo-600">Login</p>
+            </Link>
+          ) : (
+            <div>
+              <p className="text-green-500">{user?.email}</p>
+              <button
+                onClick={handleLogout}
+                className="text-indigo-500 hover:text-indigo-600 ml-2"
+              >
+                Log off
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="col-span-1 text-center">
+          <Link href="/new-product">
+            <p className="text-indigo-500 hover:text-indigo-600">Add New Product</p>
+          </Link>
+        </div>
+        <div className="col-span-1 text-center">
+          <Link href="/checkout">
+            <p className="text-indigo-500 hover:text-indigo-600">
+              Checkout
+            </p>
+            <p className="text-indigo-500 hover:text-indigo-600">
+              ({cart.length} items)
+            </p>
+          </Link>
+        </div>
+        <div className="text-center">
+          Total cart:{" "}
+          <span className="text-indigo-500">
+            {total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
